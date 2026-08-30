0536e89544d1f3f1a8a7f4ad9f8b066eac736223
Asalomedia <mediaasalo@gmail.com>
2026-08-30 21:01:49 +0300

    singgle linked list

---
+++ linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+n1=Node(12)
+n2=Node(2)
+n3=Node(42)
+n1.next=n2
+n2.next=n3
+
+def printLinkedList(n:Node):
+    currentNode=n
+    while(currentNode):
+        print(currentNode.data,end="->")
+        currentNode=currentNode.next
+    print("None")
+printLinkedList(n1)
